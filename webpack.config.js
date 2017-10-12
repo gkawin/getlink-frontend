71b535171939a94ce2d9b292c493b6bb874cc573
Kawin Glomjai <g.kawin@live.com>
2017-10-12 18:53:41 +0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const moduleRulesBabel = require('./webpack/moduleRulesBabel')
+const moduleAliases = require('./webpack/moduleAliases')
+const modulePlugins = require('./webpack/modulePlugins')
+const development = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  devtool: 'eval',
+  entry: {
+    app: path.resolve(__dirname, 'src', 'entry.js'),
+    vendor: [ 'react', 'react-dom', 'prop-types' ]
+  },
+  resolve: {
+    alias: moduleAliases
+  },
+  output: {
+    filename: '[name].bundle.js',
+    pathinfo: development
+  },
+  node: {
+    fs: 'empty'
+  },
+  module: {
+    rules: [
+      ...moduleRulesBabel
+    ]
+  },
+  plugins: modulePlugins,
+  devServer: {
+    compress: true,
+    historyApiFallback: true
+  }
+}
