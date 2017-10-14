c3d49232dff59d05d8eaebb4619e1a0564bd2a67
Kawin Glomjai <g.kawin@live.com>
2017-10-14 22:28:24 +0700

    Carousel

---
+++ src/components/CarouseImage.story.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import CarouselImage from './CarouselImage.jsx'
+
+storiesOf('CarouselImage', module)
+.add('Primary', () => (
+  <CarouselImage />
+))
