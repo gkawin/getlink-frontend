8d5ac15dbbdfaf8e43ca4b4b25df9597a0f702da
Kawin Glomjai <g.kawin@live.com>
2017-10-14 17:44:09 +0700

    Box section

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { Button } from 'react-bootstrap'
+import { Thumbnail } from 'react-bootstrap'
+import Faker from 'faker'
 
 class Box extends React.PureComponent {
   static propTypes = {
     className: PropTypes.string,
   }
   render () {
+    const imgSrc = Faker.image.image()
     return (
       <div className={this.props.className}>
-        <Button>Default</Button>
+        <Thumbnail src={imgSrc} alt='200x200' >
+          <h2 className='label-word'>{Faker.lorem.word()}</h2>
+          <p>{Faker.lorem.sentence()}</p>
+        </Thumbnail>
       </div>
     )
   }
 }
 
 export default styled(Box)`
-  width: 300px;
-  height: 300px;
-  background-color: white;
+  width: 200px;
+  height: 200px;
 `
