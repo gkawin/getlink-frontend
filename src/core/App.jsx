01c93b88efaa481afaede0e30f601f01646fab6e
Kawin Glomjai <g.kawin@live.com>
2017-10-15 12:26:57 +0700

    footer

--- src/core/App.jsx
+++ src/core/App.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Jumbotron, Button } from 'react-bootstrap'
+import styled from 'styled-components'
 
 class App extends React.PureComponent {
+  static propTypes = {
+    className: PropTypes.string,
+  }
   render () {
     return (
-      <section>
-        foo
-      </section>
+      <div className={this.props.className}>
+        <section>
+          header
+        </section>
+        <section>
+          <Jumbotron>
+            <h1>Hello, world!</h1>
+            <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <p><Button bsStyle='primary'>Learn more</Button></p>
+          </Jumbotron>
+        </section>
+        <section>
+          Content Cards
+        </section>
+        <section>
+          footer
+        </section>
+      </div>
     )
   }
 }
 
-export default App
+export default styled(App)`
+  position: fixed;
+  top: 0;
+  left: 0;
+`
